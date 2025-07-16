96c221f4a1635ce0cb06ba4a314aa78fcf1dc43d
amrnabel2025 <amr.n@amk.com.sa>
2025-07-16 11:45:26 +0300

    fix

--- src/app/[locale]/solutions/[slug]/page.tsx
+++ src/app/[locale]/solutions/[slug]/page.tsx
@@ -2,7 +2,7 @@ import SolutionDetails from "@/components/solutions/SolutionDetails";
 import { solutionsData } from "@/utils/solutionsData";
 import { notFound } from "next/navigation";
 
-// @ts-ignore: Ignore type issue in Next.js route params
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default function SolutionDetailsPage({ params }: any) {
   const solution = solutionsData.find((s) => s?.id === params?.slug);
 
