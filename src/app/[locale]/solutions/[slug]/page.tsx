3163d3960f1e7b320630b1a078209c2182f1cced
amrnabel2025 <amr.n@amk.com.sa>
2025-07-16 11:17:45 +0300

    err

--- src/app/[locale]/solutions/[slug]/page.tsx
+++ src/app/[locale]/solutions/[slug]/page.tsx
@@ -2,13 +2,11 @@ import SolutionDetails from "@/components/solutions/SolutionDetails";
 import { solutionsData } from "@/utils/solutionsData";
 import { notFound } from "next/navigation";
 
-type Props = {
-  params: {
-    slug: string;
-  };
-};
-// @ts-expect-error
-export default function SolutionDetailsPage({ params }: Props) {
+export default function SolutionDetailsPage({
+  params,
+}: {
+  params: { slug: string };
+}) {
   const solution = solutionsData.find((s) => s?.id === params?.slug);
 
   if (!solution) {
