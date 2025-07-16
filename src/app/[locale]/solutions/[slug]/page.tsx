dd345723e97598445ff5025625a3f9d47c94aa54
amrnabel2025 <amr.n@amk.com.sa>
2025-07-16 11:36:09 +0300

    build

--- src/app/[locale]/solutions/[slug]/page.tsx
+++ src/app/[locale]/solutions/[slug]/page.tsx
@@ -2,11 +2,7 @@ import SolutionDetails from "@/components/solutions/SolutionDetails";
 import { solutionsData } from "@/utils/solutionsData";
 import { notFound } from "next/navigation";
 
-export default function SolutionDetailsPage({
-  params,
-}: {
-  params: { slug: string };
-}) {
+export default function SolutionDetailsPage({ params }: any) {
   const solution = solutionsData.find((s) => s?.id === params?.slug);
 
   if (!solution) {
