2859441d03ce7ee4ef5362216e7fa3445b34a0e4
amrnabel2025 <amr.n@amk.com.sa>
2025-07-16 12:55:32 +0300

    Remove unused locale parameter and simplify message fetching in RootLayout component.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,12 @@
 import AosInitClient from "@/components/AosInitClient";
 import { NextIntlClientProvider } from "next-intl";
-import { getMessages, setRequestLocale } from "next-intl/server";
+import { getMessages } from "next-intl/server";
 
 export default async function RootLayout({
   children,
-  params,
 }: {
   readonly children: React.ReactNode;
-  readonly params: { locale: string };
 }) {
-  const { locale } = params;
-
-  setRequestLocale(locale);
-
   const messages = await getMessages();
 
   return (
